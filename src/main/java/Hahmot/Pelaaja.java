75d450b8196bc5a0ddadc75f9cce46dfd249b540
Aki <akuli91@gmail.com>
2015-05-15 20:40:55 +0300

    Lisätty Pelaajat ja viholliset

---
+++ src/main/java/Hahmot/Pelaaja.java
@@ -0,0 +1,22 @@
+package Hahmot;
+
+import java.awt.*;
+
+/**
+ * Created by Aki on 15.5.2015.
+ */
+public class Pelaaja {
+	//PelaajaHahmo skeleton
+
+	public Pelaaja() {
+	}
+
+	public void draw(Graphics g) {
+		//TODO kirjoita pelaajan piirtometodi
+	}
+
+	public Rectangle getBounds() {
+		//TODO palauta pelaajan boundit
+		return null;
+	}
+}
