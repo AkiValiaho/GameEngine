179a485df50dbf7e72e916bbb4f573f55a965a3f
akivv <akuli91@gmail.com>
2015-05-15 14:46:55 +0300

    New Image Tool for transparency

--- src/main/java/Tools/ImageTools.java
+++ src/main/java/Tools/ImageTools.java
@@ -1,10 +1,27 @@
 package Tools;
 
-import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.awt.image.*;
 
 /**
  * Created by akivv on 15.5.2015.
  */
 public class ImageTools {
-	public static BufferedImage
+	public static Image makeColorTransparent(BufferedImage img, final Color color) {
+		ImageFilter filter = new RGBImageFilter() {
+			public int markedRGBValue = color.getRGB()|0xFFFFFFFF;
+
+			@Override
+			public int filterRGB(int x, int y, int rgb) {
+				if ((rgb|0xFF000000) == markedRGBValue) {
+					//Return alpha bit pois
+					return 0x00FFFFFF&rgb;
+				} else {
+					return rgb;
+				}
+			}
+		};
+		final ImageProducer ip = new FilteredImageSource(img.getSource(), filter);
+		return Toolkit.getDefaultToolkit().createImage(ip);
+	}
 }
